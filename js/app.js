0d4aaf8bea9cedd08ca88c721ba4dbd810d6a311
darklight1236 <gamasutora@yandex.ru>
2021-01-13 23:56:42 +0400

    commit

--- js/app.js
+++ js/app.js
@@ -27,21 +27,26 @@ $('.free_cat__button').on('click', function() {
                         </div>
                         <div class="form_submit">Отправить</div>
                     </div>
-
                 </div>
-
-
             </div>
-
         </div>
   `);
 });
 
 function black_l__close(e) {
-  $('.email_form').remove();
-  $('.black_layer').remove();
+  // наличие чёрного фона
+  if($('.black_layer')){
+    $('.black_layer').remove();
+  }
+  // наличие формы для каталога
+  if($('.email_form')){
+    $('.email_form').remove();
+  }
 }
 
+$('.msg_box').on('click', function() {
+  $('.msg_form').toggleClass("msgActive");
+});
 
 // carousel
 $('.sl').slick({
@@ -54,7 +59,6 @@ $('.sl').slick({
   cssEase: 'linear'
 });
 
-
 // scroll to Top
 $(function() {
   $(window).scroll(function() {
@@ -65,7 +69,7 @@ $(function() {
   }
   });
   $('#toTop').click(function() {
-  $('body,html').animate({scrollTop:0},800);
+  $('body,html').animate({scrollTop:0},1000);
   });
 });
 
@@ -90,7 +94,6 @@ $("#header__button").on("click","a", function (event) {
   top = $(id).offset().top;
   $('body,html').animate({scrollTop: top}, 1500);
 });
-
 $("#skipToHitSales").on("click","a", function (event) {
   event.preventDefault();
   var id  = $(this).attr('href'),
@@ -98,3 +101,9 @@ $("#skipToHitSales").on("click","a", function (event) {
   $('body,html').animate({scrollTop: top}, 1500);
 });
 
+$(document).mouseup(function(e) {
+  let container = $('.msgActive');
+  if (container.has(e.target).length === 0){
+    container.removeClass('msgActive');
+  }
+})
